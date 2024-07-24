4dd0ff4c81f085da9a6c4b6304c8c23dcd7330d9
esophagoose <118142981+esophagoose@users.noreply.github.com>
2024-07-23 20:54:10 -0700

    Implement SVG schematic generation

--- components/Database.tsx
+++ components/Database.tsx
@@ -1,1 +1,84 @@
-export const kv = await Deno.openKv()
+import { convertSchematicToSvg } from "../backend/SchematicImport.tsx";
+
+export const kv = await Deno.openKv();
+
+export let user_config = JSON.parse(
+  Deno.readTextFileSync("./kicad_enterprise.json"),
+);
+
+export enum ProjectSource {
+  Git = "git",
+  Local = "local",
+}
+
+interface FileInfo {
+  name: string;
+  path: string;
+  lastModified: Date;
+}
+
+interface Schematic {
+  name: string;
+  path: string;
+  lastModified: Date;
+  svgPath: string;
+}
+
+export default interface ProjectInfo {
+  name: string;
+  path: string;
+  lastModified: Date;
+  source: ProjectSource;
+}
+
+async function parseKicadProject(location: string) {
+  const name = location.substring(location.lastIndexOf("/") + 1);
+  console.log(`Parsing Kicad project: ${name}`);
+  const project: ProjectInfo = {
+    name: name,
+    path: location,
+    lastModified: Deno.statSync(location).mtime ?? new Date(),
+    source: ProjectSource.Local,
+  };
+  await kv.set(["projects", name], project);
+  for (const file of Deno.readDirSync(location)) {
+    if (!file.name.endsWith(".kicad_sch")) {
+      continue;
+    }
+    const svgPath = convertSchematicToSvg(file.name, location);
+    const schematic: Schematic = {
+      name: file.name,
+      path: location,
+      lastModified: Deno.statSync(location).mtime ?? new Date(),
+      svgPath: svgPath,
+    };
+    const result = await kv.set([
+      "projects",
+      project.name,
+      "schematic",
+      schematic.name,
+    ], schematic);
+    console.log("  Adding schematic: ", schematic, "success: ", result.ok);
+  }
+  return true;
+}
+
+export async function update() {
+  console.log("Starting ProjectManager update!");
+  console.log("Config loaded:", user_config);
+
+  for (const location of user_config.project_locations) {
+    let isKicadProject = false;
+    console.log(`Checking location: ${location}`);
+    for (const file of Deno.readDirSync(location)) {
+      if (file.name.includes(".kicad_"))
+        isKicadProject = true;
+    }
+    if (isKicadProject) {
+      await parseKicadProject(location);
+    }
+  }
+  console.log("ProjectManager update complete.");
+}
+
+update();
--- components/ProjectManager.tsx
+++ components/ProjectManager.tsx
@@ -13,32 +13,3 @@ export default interface ProjectInfo {
   lastUpdated: Date;
   source: ProjectSource;
 }
-
-
-
-
-export async function update() {
-  console.log("Starting ProjectManager update!");
-  config = JSON.parse(Deno.readTextFileSync("./kicad_enterprise.json"));
-  console.log("Config loaded:", config);
-
-  for (const location of config.project_locations) {
-    console.log(`Checking location: ${location}`);
-    for (const file of Deno.readDirSync(location)) {
-      if (file.name.endsWith(".kicad_pcb")) {
-        console.log(`Found PCB file: ${file.name}`);
-        const project: ProjectInfo = {
-          name: file.name.split(".kicad")[0],
-          location: location,
-          lastUpdated: Deno.statSync(location).mtime ?? new Date(),
-          source: ProjectSource.Local,
-        };
-        const result = await kv.set(["projects", project.name], project);
-        console.log("Adding project: ", project, "success: ", result.ok);
-      }
-    }
-  }
-  console.log("ProjectManager update complete.");
-}
-
-update();
