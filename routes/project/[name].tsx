8b73db6ee61a43fb29b34d0d21d0f0a4751e8e31
esophagoose <118142981+esophagoose@users.noreply.github.com>
2024-07-28 13:51:53 -0700

    Async loading for schematics and removing dead code

--- routes/project/[name].tsx
+++ routes/project/[name].tsx
@@ -1,30 +1,6 @@
-import { PageProps } from "$fresh/server.ts";
-import ProjectInfo from "../../components/ProjectManager.tsx";
-import { asset, Partial } from "$fresh/runtime.ts";
 import { kv } from "../../components/Database.tsx";
 
-function bom(project) {
-  return (
-    <iframe
-      class="flex-grow"
-      src={asset("/bom/ibom.html")}
-      title="Interactive BOM for KiCAD"
-    >
-    </iframe>
-  );
-}
-
-function schematic(project) {
-  return (
-    <div class="flex-grow bg-neutral-900">
-      <script src="https://kicanvas.org/kicanvas/kicanvas.js"></script>
-      <kicanvas-embed src={asset("/sample.kicad_sch")} controls="full">
-      </kicanvas-embed>
-    </div>
-  );
-}
-
-export default async function ProjectPage(req: Request, ctx: RouteContext) {
+export default async function ProjectPage(_req: Request, ctx: RouteContext) {
   const entry = await kv.get(["projects", decodeURI(ctx.params.name)]);
 
   if (entry.value === null) {
--- routes/project/[name]/schematic.tsx
+++ routes/project/[name]/schematic.tsx
@@ -1,33 +1,48 @@
-import { JSX } from "preact/jsx-runtime";
-import { kv, SchematicInfo } from "../../../components/Database.tsx";
+import { kv } from "../../../components/Database.tsx";
 import SchView from "../../../islands/SchematicView.tsx";
 import Layout from "../../../components/Layout.tsx";
+import { PageProps } from "$fresh/server.ts";
+
+interface Project {
+  name: string;
+  schematics: Array<string>;
+  svg: string;
+}
+
+export const handler: Handlers<Project> = {
+  async GET(_req: Request, ctx: RouteContext) {
+    const project: Project = {name: "", schematics: [], svg: ""};
+    project.name = decodeURI(ctx.params.name);
+    const result = kv.list({prefix: ["schematics", project.name]});
+    let schematic = null;
+    for await (const sch of result) {
+      schematic = sch.value;
+      project.schematics.push(sch.value.name.split(".kicad_sch")[0]);
+      console.log(sch.value);
+    }
+    if (project.schematics.length == 0) {
+      return ctx.renderNotFound();
+    }
+
+    project.svg = await Deno.readTextFile(schematic.svgPath);
+    return ctx.render(project);
+  },
+};
+
 
 function createSchematicLink(name: string) {
   return (
     <li class="hover:bg-sky-800 p-4">
-      <a href={"#"}>{name.split(".kicad_sch")[0]}</a>
+      <a href={"#"}>{name}</a>
     </li>
   );
 }
-export default async function ProjectPage(_req: Request, ctx: RouteContext) {
-  const result = kv.list({
-    prefix: ["schematics", decodeURI(ctx.params.name)],
-  });
-  const schematics: Array<SchematicInfo> = [];
-  const schNavList: Array<JSX.Element> = [];
-  for await (const sch of result) {
-    schematics.push(sch.value);
-    schNavList.push(createSchematicLink(sch.value.name));
-    console.log(sch.value);
-  }
-  if (schematics.length == 0) {
-    return ctx.renderNotFound();
-  }
-  const svgContent = await Deno.readTextFile(schematics[0].svgPath);
+
+export default function ProjectPage(props: PageProps<Project>) {
+  const links = props.data.schematics.map((sch) => createSchematicLink(sch));
   return (
-    <Layout title={decodeURI(ctx.params.name)} navItems={schNavList}>
-      <SchView svg={svgContent} />
+    <Layout title={props.data.name} navItems={links}>
+      <SchView svg={props.data.svg} />
     </Layout>
   );
 }
